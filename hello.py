79863f1f2de6b3d327917e28153b727b35099044
Magdalene Isapir <isapirm@gmail.com>
2021-08-19 11:47:00 +0300

    Added more logic

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
 # created by isapirm
 print("hello world")
+
+def sum(a, b):
+    """
+    Calculate the sum of two numbers
+    """
+    return a + b
+
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number: "))
+
+total = sum(num1, num2)
+
+print(f"The sum of {num1} and {num2} = {total}")
